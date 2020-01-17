b72a2bf7b62273388e9b0351dc0b9cdae9a424a3
cubimon <cubimon93@gmail.com>
2020-01-17 19:51:15 +0100

    add spotify play next/prev

--- controller/spotify_controller.js
+++ controller/spotify_controller.js
@@ -1,6 +1,8 @@
 class SpotifyCloudController extends AbstractController {
   constructor() {
     super({
+      playPrev: '.Root__now-playing-bar > footer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > button:nth-child(1)',
+      playNext: '.Root__now-playing-bar > footer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > button:nth-child(1)',
       media: '#skMedias > audio:nth-child(1)',
       title: ['.TrackListHeader__entity-name > h2:nth-child(1) > span:nth-child(1)']
     })
