3d0167bb77be194a308d13eca8df10eba9870ee9
cubimon <cubimon93@gmail.com>
2020-01-17 19:47:54 +0100

    fix twitch

--- patches/twitch_patch.js
+++ patches/twitch_patch.js
@@ -1,5 +1,5 @@
 // expose twitch's private video player for use in this extension, to seek in videos
-patch(/.*player-core-base-.*/,
+patch(/.*core-.*/,
     'e.prototype.onStateChanged=function(e){',
     'e.prototype.onStateChanged=function(e){window.skVideoPlayer=this;')
 
