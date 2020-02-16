f22343e8ae4e8a5525c30075f9bf98b1deb8155f
cubimon <cubimon93@gmail.com>
2020-02-16 18:06:39 +0100

    build files

---
+++ web-ext-minimal-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ignoreFiles: [
+    'build.js',
+    'icons/icon.kra',
+    'mpris.js',
+    'mpris.json',
+    'package-lock.json',
+    'README.md',
+    'test',
+    'web-ext-config.js',
+    'yarn.lock'
+  ]
+}
